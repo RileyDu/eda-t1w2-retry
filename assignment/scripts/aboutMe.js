67e7a10cc6410935d71355ae33bc186395c9b436
Riley DuBord <thelifeofdu@gmail.com>
2023-11-21 23:33:14 -0600

    stretch goals done. double check done.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -131,7 +131,8 @@ switch (luckyNumber) {
 console.log(luckyResult);
 
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
-
+adventurous ? result = 'Adventures are great!' : result = 'How about we stay home?';
+console.log(result);
 
 
 
